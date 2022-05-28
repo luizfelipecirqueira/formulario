8714343c5031b4de21406df8016ef61eaa346fd8
luizfelipecirqueira <lfcs18ts@gmail.com>
2022-05-28 14:51:34 -0300

    Commit projeto formulário simples

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [name, setName] = useState('');
+  const [idade, setIdade] = useState('');
+  const handleInputName = (nameEvent: React.ChangeEvent<HTMLInputElement>) => {
+    setName(nameEvent.target.value);
+  }
+  const handleInputIdade = (idadeEvent: React.ChangeEvent<HTMLInputElement>) => {
+    setIdade(idadeEvent.target.value);
+  }
+  return (
+    <div>
+      Nome:<br />
+      <input type="text" value={name} onChange={handleInputName} />
+      <br />
+      Idade:<br />
+      <input type="number" value={idade} onChange={handleInputIdade} />
+      <hr />
+      Seu nome é: {name}
+      <br />
+      Sua idade é: {idade} anos
+    </div>
+  )
+}
+
+export default App;
