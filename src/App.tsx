9be9d330c4714d664e0206f7d4267949373b235f
unknown <lfcs18ts@gmail.com>
2022-06-14 08:28:18 -0300

    Adição do campo endereço

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import React, { useState } from 'react';
 const App = () => {
   const [name, setName] = useState('');
   const [idade, setIdade] = useState('');
+  const [endereco, setEndereco] = useState('');
   const handleInputName = (nameEvent: React.ChangeEvent<HTMLInputElement>) => {
     setName(nameEvent.target.value);
   }
   const handleInputIdade = (idadeEvent: React.ChangeEvent<HTMLInputElement>) => {
     setIdade(idadeEvent.target.value);
+  }
+  const handleInputEndereco = (enderecoEvent: React.ChangeEvent<HTMLInputElement>) => {
+    setEndereco(enderecoEvent.target.value);
   }
   return (
     <div>
@@ -17,9 +21,14 @@ const App = () => {
       Idade:<br />
       <input type="number" value={idade} onChange={handleInputIdade} />
       <hr />
+      Idade:<br />
+      <input type="text" value={endereco} onChange={handleInputEndereco} />
+      <hr />
       Seu nome é: {name}
       <br />
       Você tem: {idade} anos
+      <br />
+      Seu endereço é: {endereco}
     </div>
   )
 }
